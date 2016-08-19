176f22a3c9d8aec28f37d369bac87d9bf606488f
John Blossom <jblossom.web@gmail.com>
2016-08-19 18:28:16 -0400

    initial commit: yeoman scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name elementumMoviesApp
+ * @description
+ * # elementumMoviesApp
+ *
+ * Main module of the application.
+ */
+angular.module('elementumMoviesApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elementumMoviesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the elementumMoviesApp
+ */
+angular.module('elementumMoviesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
