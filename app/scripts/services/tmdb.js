e439fbe94f470820bf626c6951e67f31a20927e4
John Blossom <jblossom.web@gmail.com>
2016-08-21 08:45:28 -0400

    refactor service, add methods

--- app/scripts/services/tmdb.js
+++ app/scripts/services/tmdb.js
@@ -12,30 +12,58 @@ angular.module('elementumMoviesApp').service('tmdbService', ['$http', function (
   var apiBase = 'https://api.themoviedb.org/3/';
   var apiKey = 'da811d0ef4d0b95aaf27b22623b94a38';
 
-  var apiGet = function(endpoint) {
+  var apiGet = function(endpoint, querystring) {
+    if(querystring) {
+      return $http.get(apiBase+endpoint+'?'+querystring+'&api_key='+apiKey);
+    }
     return $http.get(apiBase+endpoint+'?api_key='+apiKey);
   };
 
+  this.getGenres = function() {
+    return apiGet('genre/movie/list/').then(function(result){
+      if(result.data) {
+        return result.data;
+      }
+    });
+  };
+
+  this.getGenrePage = function(id, page) {
+    page = page ? page : 1;
+    return apiGet('genre/'+id+'/movies','page='+page).then(function(result){
+      if(result.data && Number(result.data.id) === Number(id)) {
+        return result.data;
+      }
+    });
+  };
+
   this.getCollection = function(id) {
-    return apiGet('collection/'+id).then(function(collection){
-      if(collection.data && collection.data.id == id) {
-        return collection.data;
+    return apiGet('collection/'+id).then(function(result){
+      if(result.data && Number(result.data.id) === Number(id)) {
+        return result.data;
+      }
+    });
+  };
+
+  this.getList = function(id) {
+    return apiGet('list/'+id).then(function(result){
+      if(result.data && Number(result.data.id) === Number(id)) {
+        return result.data;
       }
     });
   };
 
   this.getMovie = function(id) {
-    return apiGet('movie/'+id).then(function(movie){
-      if(movie.data && movie.data.id == id) {
-        return movie.data;
+    return apiGet('movie/'+id).then(function(result){
+      if(result.data && Number(result.data.id) === Number(id)) {
+        return result.data;
       }
     });
   };
 
   this.getCredits = function(id) {
-    return apiGet('movie/'+id+'/credits').then(function(movie){
-      if(movie.data && movie.data.id == id) {
-        return movie.data;
+    return apiGet('movie/'+id+'/credits').then(function(result){
+      if(result.data && Number(result.data.id) === Number(id)) {
+        return result.data;
       }
     });
   };
